f2726827957e62f7eed3afbe3ce21c0db9b0d61e
kazenetu <rakudafactory@gmail.com>
2017-08-26 23:58:36 +0900

    アイコン追加機能を実装

--- WinGroupIcons/Form1.Designer.vb
+++ WinGroupIcons/Form1.Designer.vb
@@ -1,9 +1,9 @@
-﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
 Partial Class Form1
     Inherits System.Windows.Forms.Form
 
     'フォームがコンポーネントの一覧をクリーンアップするために dispose をオーバーライドします。
-    <System.Diagnostics.DebuggerNonUserCode()> _
+    <System.Diagnostics.DebuggerNonUserCode()>
     Protected Overrides Sub Dispose(ByVal disposing As Boolean)
         Try
             If disposing AndAlso components IsNot Nothing Then
@@ -20,11 +20,45 @@ Partial Class Form1
     'メモ: 以下のプロシージャは Windows フォーム デザイナーで必要です。
     'Windows フォーム デザイナーを使用して変更できます。  
     'コード エディターを使って変更しないでください。
-    <System.Diagnostics.DebuggerStepThrough()> _
+    <System.Diagnostics.DebuggerStepThrough()>
     Private Sub InitializeComponent()
-        components = New System.ComponentModel.Container()
+        Me.Panel1 = New System.Windows.Forms.Panel()
+        Me.addIcon = New System.Windows.Forms.Button()
+        Me.Panel1.SuspendLayout()
+        Me.SuspendLayout()
+        '
+        'Panel1
+        '
+        Me.Panel1.Anchor = CType((System.Windows.Forms.AnchorStyles.Top Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
+        Me.Panel1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle
+        Me.Panel1.Controls.Add(Me.addIcon)
+        Me.Panel1.Location = New System.Drawing.Point(491, 12)
+        Me.Panel1.Name = "Panel1"
+        Me.Panel1.Size = New System.Drawing.Size(124, 191)
+        Me.Panel1.TabIndex = 11
+        '
+        'addIcon
+        '
+        Me.addIcon.Location = New System.Drawing.Point(14, 3)
+        Me.addIcon.Name = "addIcon"
+        Me.addIcon.Size = New System.Drawing.Size(96, 23)
+        Me.addIcon.TabIndex = 12
+        Me.addIcon.Text = "アイコンの追加"
+        Me.addIcon.UseVisualStyleBackColor = True
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 12.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(627, 432)
+        Me.Controls.Add(Me.Panel1)
+        Me.Name = "Form1"
         Me.Text = "Form1"
+        Me.Panel1.ResumeLayout(False)
+        Me.ResumeLayout(False)
+
     End Sub
 
+    Friend WithEvents Panel1 As Panel
+    Friend WithEvents addIcon As Button
 End Class
--- WinGroupIcons/Form1.vb
+++ WinGroupIcons/Form1.vb
@@ -1,3 +1,67 @@
-﻿Public Class Form1
+﻿Imports System.Linq
 
+Public Class Form1
+
+    Private iconCount As Integer = 0
+
+    ''' <summary>
+    ''' ラベルのマウスイベントを追加
+    ''' </summary>
+    ''' <param name="target"></param>
+    Private Sub setHandler(target As Label)
+        AddHandler target.MouseDown, AddressOf Label_MouseDown
+        AddHandler target.MouseMove, AddressOf Label_MouseMove
+
+    End Sub
+
+    ''' <summary>
+    ''' ラベル マウスクリック
+    ''' </summary>
+    ''' <param name="sender"></param>
+    ''' <param name="e"></param>
+    Private Sub Label_MouseDown(sender As Object, e As MouseEventArgs)
+        Dim target As Label = DirectCast(sender, Label)
+
+        If e.Button = MouseButtons.Right Then
+            Me.Controls.Remove(target)
+        Else
+            target.Tag = e.Location
+        End If
+    End Sub
+
+    ''' <summary>
+    ''' ラベル マウスドラッグ
+    ''' </summary>
+    ''' <param name="sender"></param>
+    ''' <param name="e"></param>
+    Private Sub Label_MouseMove(sender As Object, e As MouseEventArgs)
+        If Not e.Button = MouseButtons.Left Then
+            Return
+        End If
+
+        Dim target As Label = DirectCast(sender, Label)
+        Dim basePoint As Point = DirectCast(target.Tag, Point)
+        target.Left = CInt((target.Left + e.Location.X - basePoint.X) / 50) * 50
+        target.Top = CInt((target.Top + e.Location.Y - basePoint.Y) / 50) * 50
+    End Sub
+
+    ''' <summary>
+    ''' アイコンの追加
+    ''' </summary>
+    ''' <param name="sender"></param>
+    ''' <param name="e"></param>
+    Private Sub addIcon_Click(sender As Object, e As EventArgs) Handles addIcon.Click
+        Dim newLabel As New Label()
+
+        iconCount += 1
+
+        newLabel.Width = 60
+        newLabel.Height = 60
+        newLabel.BackColor = Color.Black
+        newLabel.ForeColor = Color.White
+        newLabel.Text = String.Format("icon     {0}", iconCount)
+        setHandler(newLabel)
+
+        Me.Controls.Add(newLabel)
+    End Sub
 End Class
