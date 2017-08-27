39658744ef013a64a3ef255f1812d3840199a146
kazenetu <rakudafactory@gmail.com>
2017-08-27 15:38:36 +0900

    グループ判定と処理を追加

--- WinGroupIcons/IconGroups.vb
+++ WinGroupIcons/IconGroups.vb
@@ -19,21 +19,76 @@ Public Class IconGroups
     ''' <summary>
     ''' アイコンの移動
     ''' </summary>
-    ''' <param name="target"></param>
+    ''' <param name="target">移動したアイコン</param>
     Public Sub Move(ByVal target As IconInfo)
-        Dim groups = Me.Items.Where(Function(item) item.MinX > target.X).OrderBy(Function(item) item.MinX).ToList()
 
-        target.X = CInt(target.X / IconInfo.WIDTH) * IconInfo.WIDTH + 30
+        If Me.isMerge(target) Then
+            ' アイコンのグルーピング
+            For Each group In Me.Items
+                If group.isHit(target) Then
+                    Me.groupMove(target, group)
+                End If
+            Next
 
-        Dim x = target.X + IconInfo.WIDTH
-        For index = 0 To groups.Count - 1
-            groups(index).setMinX(x)
-            x += +IconInfo.WIDTH
-        Next
+            ' 空のグループは削除
+            Dim groups = Me.Items.ToList()
+            For index = 0 To groups.Count - 1
+                If Not groups(index).Items.Any() Then
+                    Me.Items.Remove(groups(index))
+                End If
+            Next
+        Else
+            ' アイコンの入れ替え
+            Dim groups = Me.Items.Where(Function(item) item.MinX > target.X).OrderBy(Function(item) item.MinX).ToList()
+
+            target.X = CInt(target.X / IconInfo.WIDTH) * IconInfo.WIDTH + 30
+
+            Dim x = target.X + IconInfo.WIDTH
+            For index = 0 To groups.Count - 1
+                groups(index).setMinX(x)
+                x += +IconInfo.WIDTH
+            Next
+        End If
 
         ' アイコンの並び直し
         Me.refresh()
     End Sub
+
+    ''' <summary>
+    ''' マージするか否か
+    ''' </summary>
+    ''' <param name="target">移動したアイコン</param>
+    ''' <returns>True:マージ</returns>
+    Private Function isMerge(ByVal target As IconInfo) As Boolean
+
+        For Each group In Me.Items
+            If group.isHit(target) Then
+                Return True
+            End If
+        Next
+
+        Return False
+    End Function
+
+    ''' <summary>
+    ''' アイコングループの移動
+    ''' </summary>
+    ''' <param name="srcIcon">対象のアイコン</param>
+    ''' <param name="descGroup">移動先のグループ</param>
+    Private Sub groupMove(ByVal srcIcon As IconInfo, ByVal descGroup As IconGroup)
+
+        ' 移動元のグループを取得
+        Dim srcGroups = Me.Items.Where(Function(group) group.Items.Contains(srcIcon))
+        If Not srcGroups.Any() Then
+            Return
+        End If
+        Dim srcGroup = srcGroups.First()
+
+        ' アイコンの移動
+        descGroup.Add(srcIcon)
+        srcGroup.Remove(srcIcon)
+    End Sub
+
 
     ''' <summary>
     ''' アイコンの更新
