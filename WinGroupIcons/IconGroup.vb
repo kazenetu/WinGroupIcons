2c9bbb5a517907f5b807c93c71fcc0708cc6d830
kazenetu <rakudafactory@gmail.com>
2017-08-27 15:38:15 +0900

    HitTestなどを追加

--- WinGroupIcons/IconGroup.vb
+++ WinGroupIcons/IconGroup.vb
@@ -18,11 +18,34 @@ Public Class IconGroup
     End Property
 
     ''' <summary>
+    ''' アイコンリストの矩形
+    ''' </summary>
+    ''' <returns></returns>
+    Public ReadOnly Property Rect As Rectangle
+        Get
+            Dim firstItem = Me.Items.First()
+            Dim minX = Me.MinX
+
+            Dim result As New Rectangle(minX, firstItem.Y, minX + Me.Items.Max(Function(item) item.X), firstItem.Y + 60)
+
+            Return result
+        End Get
+    End Property
+
+    ''' <summary>
     ''' 追加
     ''' </summary>
     ''' <param name="instance">Iconインスタンス</param>
     Public Sub Add(ByRef instance As IconInfo)
         Me.Items.Add(instance)
+    End Sub
+
+    ''' <summary>
+    ''' 削除
+    ''' </summary>
+    ''' <param name="instance">Iconインスタンス</param>
+    Public Sub Remove(ByRef instance As IconInfo)
+        Me.Items.Remove(instance)
     End Sub
 
     ''' <summary>
@@ -37,6 +60,23 @@ Public Class IconGroup
             x += IconInfo.WIDTH
         Next
     End Sub
+
+    ''' <summary>
+    ''' 指定した位置がアイコンの中か否か
+    ''' </summary>
+    ''' <param name="target">移動したアイコン</param>
+    ''' <returns>true:アイコンの中</returns>
+    Public Function isHit(ByVal target As IconInfo) As Boolean
+        Dim targetPoint = New Point(target.X, target.Y)
+
+        For Each icon In Me.Items
+            If icon IsNot target AndAlso icon.isHit(targetPoint) Then
+                Return True
+            End If
+        Next
+
+        Return False
+    End Function
 
     ''' <summary>
     ''' グループの作成
